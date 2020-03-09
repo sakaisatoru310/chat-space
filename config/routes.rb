ec015a7fbf91323e32a2b8106f0a7c696bdb57ba
sakaisatoru310 <61722988+sakaisatoru310@users.noreply.github.com>
2020-03-09 14:30:08 +0900

    Revert "Update routes"

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:new, :create, :edit, :update] do
-    resources :messages, only: [:index, :create]
-  end
+  resources :groups, only: [:index, :new, :create, :edit, :update]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
