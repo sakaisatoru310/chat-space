90e96f5b266feebef75a38e4978a551a7ca71878
sakaisatoru310 <tuba310@gmail.com>
2020-03-13 19:35:32 +0900

    デプロイ

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,6 +32,8 @@ namespace :deploy do
     invoke 'unicorn:restart'
   end
 
+  set :linked_files, %w{ config/secrets.yml }
+
   desc 'upload secrets.yml'
   task :upload do
     on roles(:app) do |host|
@@ -45,4 +47,4 @@ namespace :deploy do
   after :finishing, 'deploy:cleanup'
 end
 
-set :linked_files, %w{ config/secrets.yml }
+
